c69c5d4690932c17da5968bacb1952636bb6ae5c
Reacubeth <644327005@qq.com>
2018-12-21 15:00:20 +0800

    主页面UI修改

--- app/src/main/java/cn/xt/view/MainView.java
+++ app/src/main/java/cn/xt/view/MainView.java
@@ -23,12 +23,16 @@ public class MainView extends View {
 
     private Bitmap startBtn; //模式1
     private Bitmap startBtnMode2; //模式2
+    private Bitmap rank; //排行榜
+    private Bitmap lock; //家长锁
     private Bitmap background; // 背景图片
     private Paint paint; // 画笔
     private int screenWidth;
     private int screenHeight;
     private int x, y, w, h; // 开始按钮显示的区域
     private int x1,y1,w1,h1;
+    private int x2,y2,w2,h2;
+    private int x3,y3,w3,h3;
     private MainViewInterface listener; // 事件监听接口
 
     public MainView(Context context, AttributeSet attrs) {
@@ -36,6 +40,8 @@ public class MainView extends View {
         Resources res = this.getResources();
         startBtn = BitmapFactory.decodeResource(res, R.mipmap.mode1);
         startBtnMode2 = BitmapFactory.decodeResource(res, R.mipmap.mode2);
+        rank = BitmapFactory.decodeResource(res, R.mipmap.rank);
+        lock = BitmapFactory.decodeResource(res, R.mipmap.lock);
         // 加载背景图片
         background = BitmapFactory.decodeResource(res, R.mipmap.play_bg);
         screenHeight = DisplayUtil.getScreenHeight(context);
@@ -52,7 +58,7 @@ public class MainView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap bgBitmap, btnBitmap, btnBitmap2;
+        Bitmap bgBitmap, btnBitmap, btnBitmap2,btnBitmap3,btnBitmap4;
         // 绘制背景图片
         bgBitmap = DisplayUtil.resizeBitmap(background, screenWidth, screenHeight);
         canvas.drawBitmap(bgBitmap, 0, 0, paint);
@@ -60,16 +66,33 @@ public class MainView extends View {
         w = (int) (screenWidth * 0.5);
         h = (int) (screenHeight * 0.08);
         x = screenWidth / 2 - w / 2;
-        y = screenHeight - (int) (screenHeight * 0.68);
+        y = screenHeight - (int) (screenHeight * 0.75);
         btnBitmap = DisplayUtil.resizeBitmap(startBtn, w, h);
         canvas.drawBitmap(btnBitmap, x, y, paint);
+
         // 模式2按钮的位置
         w1 = (int) (screenWidth * 0.5);
         h1 = (int) (screenHeight * 0.08);
         x1 = screenWidth / 2 - w1 / 2;
-        y1 = screenHeight - (int) (screenHeight * 0.48);
+        y1 = screenHeight - (int) (screenHeight * 0.60);
         btnBitmap2 = DisplayUtil.resizeBitmap(startBtnMode2, w1, h1);
         canvas.drawBitmap(btnBitmap2, x1, y1, paint);
+
+        // 排行榜
+        w2 = (int) (screenWidth * 0.35);
+        h2 = (int) (screenHeight * 0.07);
+        x2 = screenWidth / 2 - w2 / 2;
+        y2 = screenHeight - (int) (screenHeight * 0.48);
+        btnBitmap3 = DisplayUtil.resizeBitmap(rank, w2, h2);
+        canvas.drawBitmap(btnBitmap3, x2, y2, paint);
+
+        // 家长锁
+        w3 = (int) (screenWidth * 0.35);
+        h3 = (int) (screenHeight * 0.07);
+        x3 = screenWidth / 2 - w3 / 2;
+        y3 = screenHeight - (int) (screenHeight * 0.35);
+        btnBitmap4 = DisplayUtil.resizeBitmap(lock, w3, h3);
+        canvas.drawBitmap(btnBitmap4, x3, y3, paint);
     }
 
     @Override
